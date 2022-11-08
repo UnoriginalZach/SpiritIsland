5ae9aeae452f87e73da567bb8f36adc1d53db103
zach f <lofi.it.guy@gmail.com>
2022-11-08 15:24:57 -0800

    added more instance variable to Area class, fixed issue in Native class

--- spiritIsland/src/Area.java
+++ spiritIsland/src/Area.java
@@ -4,18 +4,64 @@ import java.util.*;
 import src.Invaders.Invader;
 public class Area {
     private ArrayList<Invader> invaderList = new ArrayList<Invader>();
+    private ArrayList<Native> nativeList = new ArrayList<Native>();
     private String type;
     private int number;
-    private int presentNatives;
-    private int presentInvaders;
-    private int presenceLevel;
+    private boolean isNativesPresent;
+    private boolean isInvadersPresent;
+    private int presence;
+    private boolean holySite = false;
     private int blightLevel;
     private boolean protection;
     private int shieldPoints;
     // generate list to store invader types on area
     // generate method that calculates invader damage
+    public void setIsNativesPresent() {
+        if(nativeList.size() > 0) {
+            this.isNativesPresent = true;
+        }
+    }
+    public boolean getIsNativesPresent() {
+        return this.isNativesPresent;
+    }
+    public boolean getIsInvadersPresent() {
+        return this.isInvadersPresent;
+    }
 
+    public void setIsIndaversPresent() {
+        if(invaderList.size() > 0) {
+            this.isInvadersPresent = true;
+        }
+    }
     
+    public ArrayList<Native> getNativeList() {
+        return this.nativeList;
+    }
+
+    public void setNativeList(ArrayList<Native> nativeList) {
+        this.nativeList = nativeList;
+    }
+
+    public int getPresence() {
+        return this.presence;
+    }
+
+    public void setPresence(int presence) {
+        this.presence = presence;
+    }
+
+    public boolean isHolySite() {
+        return this.holySite;
+    }
+
+    public boolean getHolySite() {
+        return this.holySite;
+    }
+
+    public void setHolySite(boolean holySite) {
+        this.holySite = holySite;
+    }
+
     public Invader getInvaders() {
         for(int i = 0; i < invaderList.size(); i++) {
             if(invaderList.size() != 0) {
@@ -29,11 +75,7 @@ public class Area {
     public void addInvader(Invader invader) {
         this.invaderList.add(invader);
     }
-    public boolean isProtection() {
-        return this.protection;
-    }
-
-    public boolean getProtection() {
+    public boolean isProtected() {
         return this.protection;
     }
 
@@ -65,28 +107,6 @@ public class Area {
         this.number = number;
     }
 
-    public int getPresentNatives() {
-        return this.presentNatives;
-    }
-
-    public void setPresentNatives(int presentNatives) {
-        this.presentNatives = presentNatives;
-    }
-
-    public int getPresentInvaders() {
-        return this.presentInvaders;
-    }
-
-    public void setPresentInvaders(int presentInvaders) {
-        this.presentInvaders = presentInvaders;
-    }
-    public void setPresenceLevel(int presenceLevel) {
-        this.presenceLevel = presenceLevel;
-    }
-    public int getPresenceLevel(int presenceLevel) {
-        return this.presenceLevel;
-    }
-    
     public int getBlightLevel() {
         return this.blightLevel;
     }
@@ -95,20 +115,8 @@ public class Area {
         this.blightLevel = blightLevel;
     }
     public void attackPhase(int addedSpiritPower) {
-        int totalPower = this.presentNatives + addedSpiritPower;
-        this.presentInvaders = this.presentInvaders - totalPower;
-        if(this.presentInvaders < 0) {
-            this.presentInvaders = 0;
-        }
-        if(!protection){
-        this.presentNatives = this.presentNatives - this.presentInvaders;
-        if(presentNatives < 0) {
-            this.presentNatives = 0;
-            this.blightLevel += 1;
-        }
+        
         }
         
-        
-    }
 
 }
--- spiritIsland/src/Native.java
+++ spiritIsland/src/Native.java
@@ -2,11 +2,18 @@ package src;
 public class Native {
     private int attackPoints = 2;
     private int lifePoints = 2;
+    private int shieldPoints;
 
     public int getLifePoints() {
         return this.lifePoints;
     }
     public int getAttackPoints() {
         return this.attackPoints;
+    }
+    public void setShieldPoints(int shieldPoints) {
+        this.shieldPoints = shieldPoints;
+    }
+    public int getShieldPoints() {
+        return this.shieldPoints;
     }
 }
