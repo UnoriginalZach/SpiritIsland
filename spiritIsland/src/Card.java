79dd4bbfc12fa42e594a3bd8a71bd8bd8eca290a
zach f <lofi.it.guy@gmail.com>
2022-11-26 14:41:39 -0800

    updated card class, updated Card management by Player class

--- spiritIsland/src/Card.java
+++ spiritIsland/src/Card.java
@@ -7,7 +7,54 @@ public class Card {
     private String name;
     private String description;
     private int range;
-    private ArrayList elements = new ArrayList<>();
+    private ArrayList<String> elements = new ArrayList<String>(8);
     private String speed;
+    public Card (int cost, String name, String description, int range,String speed, String element) {
+        this.cost = cost;
+        this.name = name;
+        this.description = description;
+        this.range = range;
+        this.speed = speed;
+        this.elements.add(element.toLowerCase());
+    }
 
+    public int getCost() {
+        return this.cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getRange() {
+        return this.range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void setElements(ArrayList<String> elements) {
+        this.elements = elements;
+    }
+    public ArrayList<String> getElements() {
+        return elements;
+    }
+
+    public String getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+    
 }
--- spiritIsland/src/Player.java
+++ spiritIsland/src/Player.java
@@ -6,9 +6,11 @@ public class Player {
     private String name;
     private Spirits spirit;
     private int energy;
-    private ArrayList discard = new ArrayList<>();
-    private ArrayList elementsPlayed = new ArrayList<>();
-    private ArrayList hand = new ArrayList<>();
+    private ArrayList<Card> discard = new ArrayList<Card>();
+    private ArrayList<Card> elementsPlayed = new ArrayList<Card>();
+    private ArrayList<Card> hand = new ArrayList<Card>();
+    private ArrayList<Card> cardsPlayed = new ArrayList<Card>();
+
     public int getEnergy() {
         return this.energy;
     }
@@ -31,5 +33,74 @@ public class Player {
 
     public void setSpirit(Spirits spirit) {
         this.spirit = spirit;
+    }
+    public void getHand() {
+        for(int i = 0; i < hand.size() -1; i ++) {
+            System.out.println(hand.get(i));
+        }
+    }
+    public void getDiscard() {
+        for(int i = 0; i < discard.size() -1; i ++) {
+            System.out.println(discard.get(i));
+        }
+    }
+    public void getElements() {
+        for(int i = 0; i < elementsPlayed.size() -1; i ++) {
+            System.out.println(elementsPlayed.get(i));
+        }
+    }
+    public void addCardToHand(Card card) {
+        hand.add(card);
+    }
+    public void DiscardCard (Card card) {
+        discard.add(card);
+    }
+    public void getTotalElementsPlayed(ArrayList<Card> cardsPlayed) {
+        int totalWater = 0;
+        int totalFire = 0;
+        int totalEarth = 0;
+        int totalAir = 0;
+        int totalPlant = 0;
+        int totalAnimal = 0;
+        int totalMoon = 0;
+        int totalSun = 0;
+        for(int i = 0; i < cardsPlayed.size() - 1; i ++) {
+            for(int j = 0; j <cardsPlayed.get(i).getElements().size() -1; i ++) {
+                switch(cardsPlayed.get(i).getElements().get(j)) {
+                    case "water":
+                        totalWater++;
+                        break;
+                    case "fire":
+                        totalFire++;
+                        break;
+                    case "earth":
+                        totalEarth++;
+                        break;
+                    case "air":
+                        totalAir++;
+                        break;
+                    case "plant":
+                        totalPlant++;
+                        break;
+                    case "animal":
+                        totalAnimal++;
+                        break;
+                    case "moon":
+                        totalMoon++;
+                        break;
+                    case "sun":
+                        totalSun++;
+                        break;
+                }
+            }
+        }
+        System.out.println("total water obtained this round is: "+totalWater);
+        System.out.println("total fire obtained this round is: "+totalFire);
+        System.out.println("total earth obtained this round is: "+totalEarth);
+        System.out.println("total air obtained this round is: "+totalAir);
+        System.out.println("total plant obtained this round is: "+totalPlant);
+        System.out.println("total animal obtained this round is: "+totalAnimal);
+        System.out.println("total sun obtained this round is: "+totalSun);
+        System.out.println("total moon obtained this round is: "+totalMoon);
     }
 }
